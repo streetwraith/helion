4fffb424beb67d53a6ec4378ad6bad26da318099
streetwraith <streetwraith@znik.net>
2025-03-11 13:50:07 +0100

    added some extra celery debug

--- market/middleware.py
+++ market/middleware.py
@@ -1,6 +1,12 @@
 from django.utils.deprecation import MiddlewareMixin
+from celery.app.control import Inspect
+from helion.celery import app
 
 class LogRequestMiddleware(MiddlewareMixin):
     def process_request(self, request):
         print(f"Handling request path: {request.path}")
+
+        inspect = app.control.inspect()
+        active_tasks = inspect.active()
+        print(f'active_tasks: {active_tasks}')
         return None
